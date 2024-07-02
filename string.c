68a5c5e5f76f62a7a3fe5e0b532d6c9f792d056c
indirapriya-11 <indirapriya0816@gmail.com>
2024-07-02 16:50:15 +0530

    Printing Name

---
+++ string.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	char str[10];
+	printf("Enter Your Name \n");
+	scanf("%s",str);
+	printf("Hello %s \n",str);
+	return 0;
+}
